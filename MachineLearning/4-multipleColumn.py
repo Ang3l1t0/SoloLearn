a3fae68195ab289d61e3585f8f2ef56ed0963309
Ang3l1t0 <aepalma4712@gmail.com>
2021-11-05 18:06:31 -0500

    commit

--- MachineLearning/4-multipleColumn.py
+++ MachineLearning/4-multipleColumn.py
@@ -3,3 +3,9 @@ import pandas as pd
 df = pd.read_csv("titanic.csv")
 small_df = df[['Age', 'Sex', 'Survived']]
 print(small_df.head())
+
+'''
+When selecting a single column from a Pandas DataFrame, 
+we use single square brackets. When selecting multiple columns, 
+we use double square brackets.
+'''
